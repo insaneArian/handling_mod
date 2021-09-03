12140669298f517e1296f38c07dc1ccfd1848ee8
Hiso <dead.inside.me@gmail.com>
2021-09-03 11:34:41 -0300

    Update from __resource to fxmanifest
    Don't be lazy, start using fxmanifest.lua instead of deprecated __resource.lua
    
    😘

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+game 'gta5'
+description 'Handling.meta edition for most cars'
+author 'insaneArian'
+version '1.0.0'
+
+data_file 'HANDLING_FILE' 'handling.meta'
+
+files {
+	'handling.meta'
+}
